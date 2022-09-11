ebec5807131d8e1cb993377bfa915eeed8014f69
Arshia <35062789+Arshiaprg@users.noreply.github.com>
2022-09-11 16:12:41 +0430

    Fix Javascript Files

--- web/js/connector.js
+++ web/js/connector.js
@@ -44,23 +44,3 @@ async function set_variable(name,value) {
     let n = await eel.set_variable(name,value)();
     return n;
 }
-
-// Example Of Creating Box
-createBox(2,2).then((box_id) => {
-    attachBox(box_id,null,0,2);
-    return box_id;
-}).then((box_id) => {
-    attachBox(box_id,null,1,1);
-    return box_id;
-}).then((box_id) => {
-    get_outputs(box_id).then((result) => {
-        console.log(result);
-    });
-});
-
-// If You Want To Use An Extention Box, You Need To Have Box Names And Extention Name.
-define_variable("test",1).then((variable) => {
-    run_box(variable[0]).then((val) => {
-        console.log(val);
-    });
-});
